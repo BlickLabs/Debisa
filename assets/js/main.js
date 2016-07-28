335173ae77ac67cdbe1c2265b1537cd6280c1d71
Alan Sanchez <sanchezpineda03@gmail.com>
2016-07-28 17:58:23 -0500

    Modified default validator error messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,25 @@ $('.header-container, .borders, .click-scrolling').click(function () {
 $('#navbar-trigger').click(function () {
     $('#navbar-collapse').addClass('in');
 });
+jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo es obligatorio",
+    remote: "Please fix this field",
+    email: "Ingresa una dirección de correo válida",
+    url: "Ingresa una URL válida",
+    date: "Ingresa una fecha válida",
+    dateISO: "Ingresa una fecha válida (ISO)",
+    number: "Ingresa un número válido",
+    digits: "Solo se permiten números dígitos",
+    creditcard: "Ingresa un número de tarjeta válido",
+    equalTo: "Los valores deben coincidir",
+    accept: "Please enter a value with a valid extension",
+    maxlength: jQuery.validator.format("No ingreses más de {0} caracteres"),
+    minlength: jQuery.validator.format("Ingresa al menos {0} caracteres"),
+    rangelength: jQuery.validator.format("El texto debe tener entre {0} y {1} caracteres"),
+    range: jQuery.validator.format("Ingresa un valor entre {0} y {1}"),
+    max: jQuery.validator.format("Ingresa un valor menor o igual que {0}"),
+    min: jQuery.validator.format("Ingresa un valor mayor o igual que {0}")
+});
 $('#contact-modal form').validate({
     rules: {
         contact_name: {
